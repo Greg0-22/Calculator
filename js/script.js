073d2ba426d2f38951dab3819acf7782d694607f
Greg0-22 <g.lelandjr@gmail.com>
2023-03-06 22:10:25 -0600

    Calculation functions made

---
+++ js/script.js
@@ -0,0 +1,41 @@
+function add(n1, n2){
+    let total = n1 + n2;
+    return total;
+}
+
+function subtract(n1, n2){
+    let difference = n1 - n2;
+    return difference;
+}
+
+function multiply(n1, n2){
+    let product = n1 * n2;
+    return product;
+}
+
+function divide(n1, n2){
+    if(n2 === 0){
+        return "Can't divide by 0."
+    } else {
+        return n1 / n2;;
+    }
+}
+
+function operate(str, n1, n2){
+    switch (str){
+        case '+':
+            console.log(add(n1, n2));
+            break;
+        case '-':
+            console.log(subtract(n1, n2));
+            break;
+        case '*':
+            console.log(multiply(n1, n2));
+            break;
+        case '/':
+            console.log(divide(n1, n2));
+            break;
+        default:
+            console.log(`Invalid operator: ${str}`);
+    }
+}
