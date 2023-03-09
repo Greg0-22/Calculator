49aaa8ad0d7730d888dce76a7acc7e4d0c672e25
Greg0-22 <g.lelandjr@gmail.com>
2023-03-08 22:04:06 -0600

    Changes to HTML structure and attributes. Added buttons event listener and interactions with display.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,51 @@
+let userNum1
+let userOperand1
+let userNum2
+let userOperand2
+const display = document.querySelector('#calc-display');
+const buttons = document.querySelector('#buttons');
 
+// button listener
+buttons.addEventListener('click', function(event){
+    if (event.target.matches('button')){
+        const button = event.target;
+        const action = button.dataset.action;
+        const displayNum = display.textContent;
+        const buttonNum = button.textContent;
 
+        //remove button press class when another button is pressed.
+        Array.from(button.parentNode.children).forEach(button => button.classList.remove('clicked-down'));
+        // 0-9 number buttons
+        if(!action){
+            // if display is 0 replace with button #, but if not then tack button # onto end of display #
+            if (displayNum === '0'){
+                display.textContent = buttonNum;
+            } else {
+                display.textContent = displayNum + buttonNum;
+            }
+        }
 
+        // * / + - operator buttons
+        if(button.matches('.operator')){
+            button.classList.add('clicked-down')
+        }
 
+        // . AC = decimal, clear, equals buttons
+        if(action == 'decimal'){
+            display.textContent = displayNum + '.'
+        }
 
+        if(action == 'clear'){
+            console.log('clear');
+        }
 
+        if(action == 'equals'){
+            console.log('equals');
+        }
 
-
-
+        
+    }
+});
 
 // calculator functions
 
