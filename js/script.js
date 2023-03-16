d51e37019aff3902324a4824a126f69122a9f2fd
Greg0-22 <g.lelandjr@gmail.com>
2023-03-15 21:01:04 -0500

    need to finish edge cases, calculations working. created working branch

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let userNum2
 let userOperand2
 const display = document.querySelector('#calc-display');
 const buttons = document.querySelector('#buttons');
+const calculator = document.querySelector('#calculator');
 
 // button listener
 buttons.addEventListener('click', function(event){
@@ -12,14 +13,16 @@ buttons.addEventListener('click', function(event){
         const action = button.dataset.action;
         const displayNum = display.textContent;
         const buttonNum = button.textContent;
+        const previousButtonType = calculator.dataset.previousButtonType;
 
         //remove button press class when another button is pressed.
         Array.from(button.parentNode.children).forEach(button => button.classList.remove('clicked-down'));
         // 0-9 number buttons
         if(!action){
             // if display is 0 replace with button #, but if not then tack button # onto end of display #
-            if (displayNum === '0'){
+            if (displayNum === '0' || previousButtonType === 'operator'){
                 display.textContent = buttonNum;
+                calculator.dataset.previousButtonType = '';
             } else {
                 display.textContent = displayNum + buttonNum;
             }
@@ -28,11 +31,16 @@ buttons.addEventListener('click', function(event){
         // * / + - operator buttons
         if(button.matches('.operator')){
             button.classList.add('clicked-down')
+            calculator.dataset.previousButtonType = 'operator'
+            calculator.dataset.firstValue = displayNum
+            calculator.dataset.operator = action
         }
 
         // . AC = decimal, clear, equals buttons
         if(action == 'decimal'){
-            display.textContent = displayNum + '.'
+            if(!displayNum.includes('.')){
+                display.textContent = displayNum + '.'
+            };
         }
 
         if(action == 'clear'){
@@ -40,7 +48,11 @@ buttons.addEventListener('click', function(event){
         }
 
         if(action == 'equals'){
-            console.log('equals');
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayNum
+
+            display.textContent = operate(operator, firstValue, secondValue);
         }
 
         
@@ -50,43 +62,49 @@ buttons.addEventListener('click', function(event){
 // calculator functions
 
 function add(n1, n2){
-    let total = n1 + n2;
+    let total = parseFloat(n1) + parseFloat(n2);
     return total;
 }
 
 function subtract(n1, n2){
-    let difference = n1 - n2;
+    let difference = parseFloat(n1) - parseFloat(n2);
     return difference;
 }
 
 function multiply(n1, n2){
-    let product = n1 * n2;
+    let product = parseFloat(n1) * parseFloat(n2);
     return product;
 }
 
 function divide(n1, n2){
-    if(n2 === 0){
+    if(n2 === '0'){
         return "Can't divide by 0."
     } else {
-        return n1 / n2;;
+        return parseFloat(n1) / parseFloat(n2);;
     }
 }
 
 function operate(str, n1, n2){
+    let result;
     switch (str){
-        case '+':
+        case 'addition':
             console.log(add(n1, n2));
+            result = add(n1, n2)
             break;
-        case '-':
+        case 'subtract':
             console.log(subtract(n1, n2));
+            result = subtract(n1, n2)
             break;
-        case '*':
+        case 'multiply':
             console.log(multiply(n1, n2));
+            result = multiply(n1, n2)
             break;
-        case '/':
+        case 'division':
             console.log(divide(n1, n2));
+            result = divide(n1, n2)
             break;
         default:
             console.log(`Invalid operator: ${str}`);
     }
+    return result;
 }
